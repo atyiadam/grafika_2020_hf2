cc5476bb5e7857e573202488827963652dc20fc3
Atyi Ádám <atyiadam@gmail.com>
2020-04-13 16:53:04 +0200

    Initial Commit

---
+++ grafika_2020_hf2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  grafika_2020_hf2
+//
+//  Created by Ádám Atyi on 2020. 04. 13..
+//  Copyright © 2020. Ádám Atyi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
